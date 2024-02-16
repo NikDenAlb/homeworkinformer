ddb6008fe219e755d737c97f5c292ebb0d06afb6
NuzGooL <ardeniaanderland@gmail.com>
2024-02-16 10:13:23 +0300

    Initial commit

---
+++ src/test/java/coursework/three/homeworkInformer/HomeworkInformerApplicationTests.java
@@ -0,0 +1,13 @@
+package coursework.three.homeworkInformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkInformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
